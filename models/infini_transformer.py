be2e2ee0c2f3626ef9f9239a53a9d02caeeea0ad
quantumiracle <zhding96@gmail.com>
2025-02-11 14:09:49 -0500

    infini attention ready to run

--- models/infini_transformer.py
+++ models/infini_transformer.py
@@ -5,7 +5,7 @@ from math import ceil
 from copy import deepcopy
 from functools import partial
 from collections import namedtuple
-
+from torch import einsum
 import tqdm
 
 import torch
@@ -32,6 +32,7 @@ def min_p_filter(logits, min_p = 0.1):
     limit = min_p * max_probs
     return torch.where(probs < limit, float('-inf'), logits)
 
+
 # sampling related
 
 def log(t, eps = 1e-20):
@@ -46,6 +47,43 @@ def gumbel_sample(t, temperature = 1.):
         t = t / temperature + gumbel_noise(t)
     return t.argmax(dim = -1, keepdim = True)
 
+
+
+def round_up_multiple(seq, mult):
+    return ceil(seq / mult) * mult
+
+def pad_and_segment_with_inverse(
+    seq,
+    segment_len,
+    fold_into_batch = True,
+    inverse_remove_pad = True
+):
+    batch, seq_len = seq.shape[:2]
+    next_seq_len_mult = round_up_multiple(seq_len, segment_len)
+
+    padding = next_seq_len_mult - seq_len
+    needs_pad = padding > 0
+
+    if needs_pad:
+        seq = F.pad(seq, (0, 0, 0, padding))
+
+    if fold_into_batch:
+        seq = rearrange(seq, 'b (w n) d -> (b w) n d', n = segment_len)
+
+    def inverse(out):
+
+        if fold_into_batch:
+            out = rearrange(out, '(b w) ... n d -> b ... (w n) d', b = batch)
+
+        if needs_pad and inverse_remove_pad:
+            out = out[..., :-padding, :]
+
+        return out
+
+    return seq, inverse
+
+def sigma_act(x):
+    return F.elu(x) + 1
 
 class FeedForward(nn.Module):
     def __init__(self, dim, hidden_dim):
@@ -64,8 +102,79 @@ class GEGLU(Module):
         x, gate = x.chunk(2, dim = -1)
         return F.silu(gate) * x
 
-class Attention(nn.Module):
-    def __init__(self, dim, heads = 8, dim_head = 64, is_causal = True):
+def updateMemory(kv_mem, k, v, z, delta=False):
+    if kv_mem is not None:
+        assert kv_mem.shape[1] == v.shape[1], "Memory length must be the same as the number of new keys"
+    if delta and (kv_mem is not None):
+        sigma_k = sigma_act(k)
+        numerator = einsum(
+            "b h n k, b h k v -> b h n v",
+            sigma_k,
+            kv_mem,
+        )
+        denominator = einsum(
+            "b h n k, b h k -> b h n",
+            sigma_k,
+            z,
+        )
+        denominator = rearrange(
+            denominator,
+            "b h n -> b h n 1",
+        )
+        prev_v = numerator / denominator
+        new_value_states = v - prev_v
+        new_kv_mem = torch.matmul(sigma_k.transpose(-2, -1), new_value_states)
+    else:
+        k_T = rearrange(sigma_act(k), "b h n d -> b h d n")
+        new_kv_mem = sigma_act(k_T) @ v
+    if kv_mem is not None:
+        new_kv_mem = kv_mem + new_kv_mem
+    return new_kv_mem
+
+def getAttnMem(kv_mem, z, q):
+    assert kv_mem is not None, "Attention memory must be provided"
+    sigma_q = sigma_act(q)
+    retrieved_memory = einsum(
+        "b h n k, b h k v -> b h n v",
+        sigma_q,
+        kv_mem,
+    )
+    denominator = einsum(
+        "b h n k, b h k -> b h n",
+        sigma_q,
+        z,
+    )
+    denominator = rearrange(
+        denominator,
+        "b h n -> b h n 1",
+    )
+    retrieved_memory = retrieved_memory / (denominator + 1e-6)  # Adding 1e-6 for preventing division to 0
+    return retrieved_memory
+
+
+def updateZ(z, k):
+    # k: (b, h, n, d)
+    k = sigma_act(k)
+    new_z = torch.sum(k, dim=2, keepdim=False)
+    if z is not None:
+        new_z = z + new_z
+    return new_z #
+
+class InfiniAttention(nn.Module):
+    """
+    InfiniAttention is a variant of the Attention class that allows for fixed length of the cached keys and values as memory.
+    https://arxiv.org/pdf/2404.07143
+    """
+    def __init__(
+        self, 
+        dim, 
+        heads = 8, 
+        dim_head = 64, 
+        is_causal = True, 
+        bptt = False, 
+        delta_update = True,
+        segment_len = 32,
+    ):
         super().__init__()
         inner_dim = dim_head *  heads
         self.heads = heads
@@ -73,13 +182,26 @@ class Attention(nn.Module):
         self.norm = nn.LayerNorm(dim)
         self.rotary_emb = RotaryEmbedding(dim_head)
         self.attend = nn.Softmax(dim = -1)
+        self.dim_head = dim_head
 
         self.is_causal = is_causal
-
+        self.bptt = bptt
+        self.delta_update = delta_update
+        self.segment_len = segment_len
         self.to_qkv = nn.Linear(dim, inner_dim * 3, bias = False)
         self.to_out = nn.Linear(inner_dim, dim, bias = False)
 
-    def forward(self, x, cache = None):
+        self.device = torch.device("cuda")
+
+        # memory params
+        self.mem_beta = nn.Parameter(torch.zeros(1, heads, 1, 1)).to(self.device)
+
+    def forward(self, x, cache = None, memory_cache = None):
+        batch = x.shape[0]
+
+        # auto pad to multiple
+        x, inverse_segment = pad_and_segment_with_inverse(x, self.segment_len, fold_into_batch = False)
+
         x = self.norm(x)
 
         qkv = self.to_qkv(x).chunk(3, dim = -1)
@@ -90,30 +212,57 @@ class Attention(nn.Module):
             past_k, past_v = cache
             k = torch.cat((past_k, k), dim = -2)
             v = torch.cat((past_v, v), dim = -2)
-
-        new_cache = (k, v)
+        next_cache = tuple(map(inverse_segment, (k, v)))
 
         # relative positions
         q, k = self.rotary_emb.rotate_queries_with_cached_keys(q, k)
 
-        # causal mask
-        if self.is_causal:
-            seq_len = q.shape[-2]
-            causal_mask = torch.triu(torch.ones((seq_len, seq_len), device=x.device), diagonal=1).bool()
+        # segment
+        q, k, v = tuple(rearrange(t, 'b h (w n) d -> w b h n d', n = self.segment_len) for t in (q, k, v))
 
-        # attention
-        dots = torch.matmul(q, k.transpose(-1, -2)) * self.scale
-        if self.is_causal:
-            dots.masked_fill_(causal_mask, float('-inf'))
+        # initialize memory and z
+        if memory_cache is None:
+            mem = torch.zeros(1, self.heads, self.dim_head, self.dim_head).to(self.device)
+            z = torch.zeros(1, self.heads, self.dim_head).to(self.device)
+        else:
+            mem, z = memory_cache
 
-        attn = self.attend(dots)
+        outputs = []
+        for i in range(k.shape[0]):
+            q_slice = q[i]
+            k_slice = k[i]
+            v_slice = v[i]
 
-        out = torch.matmul(attn, v)
-        out = rearrange(out, 'b h n d -> b n (h d)')
-        
-        return self.to_out(out), new_cache
+            # a simpler variant for attention computation
+            out = F.scaled_dot_product_attention(query=q_slice, key=k_slice, value=v_slice, is_causal=self.is_causal)
 
-class Transformer(Module):
+            # memory retrieval
+            mem_out = getAttnMem(mem, z, q_slice)
+            out = F.sigmoid(self.mem_beta) * mem_out + (1 - F.sigmoid(self.mem_beta)) * out
+
+            outputs.append(out)
+
+            # memory update
+            if self.bptt:
+                new_mem = updateMemory(mem, k_slice, v_slice, z, self.delta_update)
+                new_z = updateZ(z, k_slice)
+            else:
+                with torch.no_grad():
+                    new_mem = updateMemory(mem, k_slice, v_slice, z, self.delta_update)
+                    new_z = updateZ(z, k_slice)
+
+        out = torch.stack(outputs)
+        out = rearrange(out, 'w b h n d -> (w b) n (h d)', b = batch)
+
+        out = self.to_out(out)
+        out = rearrange(out, '(w b) n d -> b (w n) d', b = batch)
+
+        out = inverse_segment(out)
+
+        return out, next_cache
+
+
+class InfiniTransformer(Module):
     def __init__(
         self, 
         num_tokens,
@@ -122,6 +271,7 @@ class Transformer(Module):
         heads, 
         dim_head, 
         mlp_dim,
+        segment_len,
         token_emb: Module | None = None,
         ):
         super().__init__()
@@ -134,7 +284,7 @@ class Transformer(Module):
         self.layers = nn.ModuleList([])
         for _ in range(depth):
             self.layers.append(nn.ModuleList([
-                Attention(dim, heads = heads, dim_head = dim_head),
+                InfiniAttention(dim, heads = heads, dim_head = dim_head, segment_len = segment_len),
                 FeedForward(dim, mlp_dim)
             ]))
 
---
+++ train_infini.py
@@ -0,0 +1,122 @@
+import random
+import tqdm
+import gzip
+import numpy as np
+
+import torch
+from torch import nn, Tensor
+from torch.nn import functional as F
+from torch.utils.data import DataLoader, Dataset
+
+from torch.optim import Adam
+from adam_atan2_pytorch import AdoptAtan2
+
+from models.infini_transformer import (
+    InfiniTransformer,
+)
+import os
+os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+
+# constants
+
+SAVE_DIR = './saved_models'
+SAVE_FILENAME = 'infini_transformer.pt'
+NUM_BATCHES = int(1e5)
+BATCH_SIZE = 4
+GRADIENT_ACCUMULATE_EVERY = 4
+LEARNING_RATE = 2e-4
+VALIDATE_EVERY  = 100
+GENERATE_EVERY  = 500
+PRIME_LENGTH = 100
+GENERATE_LENGTH = 512
+SHOULD_GENERATE = True
+USE_FAST_INFERENCE = False
+SEQ_LEN = 512
+
+# helpers
+def cycle(loader):
+    while True:
+        for data in loader:
+            yield data
+
+def decode_token(token):
+    return str(chr(max(32, token)))
+
+def decode_tokens(tokens):
+    return ''.join(list(map(decode_token, tokens)))
+
+
+# initialize transformer
+model = InfiniTransformer(
+    num_tokens = 256,
+    dim = 384,
+    depth = 8,
+    heads = 8,
+    dim_head = 64,
+    mlp_dim = 512,
+    segment_len = 32,  # segment for memory
+    ).cuda()
+
+# prepare enwik8 data
+
+with gzip.open('./data/enwik8.gz') as file:
+    data = np.frombuffer(file.read(int(95e6)), dtype = np.uint8).copy()
+    data_train, data_val = np.split(data, [int(90e6)])
+    data_train, data_val = map(torch.from_numpy, (data_train, data_val))
+
+class TextSamplerDataset(Dataset):
+    def __init__(self, data, seq_len):
+        super().__init__()
+        self.data = data
+        self.seq_len = seq_len
+
+    def __getitem__(self, index):
+        rand_start = torch.randint(0, self.data.size(0) - self.seq_len, (1,))
+        full_seq = self.data[rand_start: rand_start + self.seq_len + 1].long()
+        return full_seq.cuda()
+
+    def __len__(self):
+        return self.data.size(0) // self.seq_len
+
+train_dataset = TextSamplerDataset(data_train, SEQ_LEN)
+val_dataset   = TextSamplerDataset(data_val, SEQ_LEN)
+train_loader  = cycle(DataLoader(train_dataset, batch_size = BATCH_SIZE))
+val_loader    = cycle(DataLoader(val_dataset, batch_size = BATCH_SIZE))
+
+# optimizer
+
+optim = AdoptAtan2(model.parameters(), lr = LEARNING_RATE)
+
+# training
+
+os.makedirs(SAVE_DIR, exist_ok=True)
+for i in tqdm.tqdm(range(NUM_BATCHES), mininterval = 10., desc = 'training'):
+    model.train()
+
+    for __ in range(GRADIENT_ACCUMULATE_EVERY):
+        loss = model(next(train_loader), return_loss = True)
+        loss.backward()
+
+    print(f'training loss: {loss.item()}')
+    torch.nn.utils.clip_grad_norm_(model.parameters(), 0.5)
+    optim.step()
+    optim.zero_grad()
+
+    if i % VALIDATE_EVERY == 0:
+        model.eval()
+        with torch.no_grad():
+            loss = model(next(val_loader), return_loss = True)
+            print(f'validation loss: {loss.item()}')
+
+    if SHOULD_GENERATE and i % GENERATE_EVERY == 0:
+        model.eval()
+        inp = random.choice(val_dataset)[:PRIME_LENGTH]
+        prime = decode_tokens(inp)
+        print(f'%s \n\n %s', (prime, '*' * 100))
+
+        sample = model.sample(inp[None, ...], GENERATE_LENGTH, use_cache = USE_FAST_INFERENCE)
+        output_str = decode_tokens(sample[0])
+        print(output_str)
+
+model_save_path = os.path.join(SAVE_DIR, SAVE_FILENAME)
+torch.save(model.state_dict(), model_save_path) 
