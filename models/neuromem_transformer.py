85373cd81d5e9b20f6e5a8d5b5a742c47deb62a6
quantumiracle <1402434478@qq.com>
2025-04-22 02:13:58 +0000

    add transformer with neural memory; one token a time at training (slow)

---
+++ models/neuromem_transformer.py
@@ -0,0 +1,346 @@
+from __future__ import annotations
+from typing import Callable
+
+from math import ceil
+from copy import deepcopy
+from functools import partial
+from collections import namedtuple
+
+import tqdm
+from typing import Optional
+import torch
+from torch import nn, stack, cat
+import torch.nn.functional as F
+from torch.nn import Module, ModuleList, Linear
+from einops import repeat, rearrange, pack, unpack
+from einops.layers.torch import Rearrange
+from rotary_embedding_torch import RotaryEmbedding
+
+LinearNoBias = partial(Linear, bias = False)
+
+# helpers
+
+def exists(v):
+    return v is not None
+
+# min_p
+# https://arxiv.org/abs/2407.01082
+
+def min_p_filter(logits, min_p = 0.1):
+    probs = logits.softmax(dim = -1)
+    max_probs = probs.amax(dim = -1, keepdim = True)
+    limit = min_p * max_probs
+    return torch.where(probs < limit, float('-inf'), logits)
+
+# sampling related
+
+def log(t, eps = 1e-20):
+    return torch.log(t.clamp(min = eps))
+
+def gumbel_noise(t):
+    noise = torch.rand_like(t)
+    return -log(-log(noise))
+
+def gumbel_sample(t, temperature = 1.):
+    if temperature > 0.:
+        t = t / temperature + gumbel_noise(t)
+    return t.argmax(dim = -1, keepdim = True)
+
+
+class FeedForward(nn.Module):
+    def __init__(self, dim, hidden_dim):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.LayerNorm(dim),
+            nn.Linear(dim, hidden_dim),
+            nn.GELU(),
+            nn.Linear(hidden_dim, dim),
+        )
+    def forward(self, x):
+        return self.net(x)
+
+class GEGLU(Module):
+    def forward(self, x):
+        x, gate = x.chunk(2, dim = -1)
+        return F.silu(gate) * x
+    
+
+class NeuralMemory(nn.Module):
+    def __init__(self, input_dim: int, output_dim: int, mem_dim: int, hidden_dim: int, stride: int):
+        super().__init__()
+        self.mem_dim = mem_dim
+        self.stride = stride
+        self.temporal_rotary_emb = RotaryEmbedding(dim=input_dim)
+
+        self.update_net = nn.Sequential(
+            nn.Linear(2 * input_dim + mem_dim, hidden_dim),
+            nn.GELU(),
+            nn.Linear(hidden_dim, mem_dim)
+        )
+
+        # Memory retrieval network (e.g., dot-product attention)
+        self.retrieve_key_proj = nn.Linear(mem_dim, input_dim)
+        self.retrieve_val_proj = nn.Linear(mem_dim, input_dim)
+        self.proj_to_token_dim = nn.Linear(input_dim, output_dim)
+
+    def forward_update(self, k, v, prev_mem=None):
+        B, H, N, D = k.shape
+        k = k[:, :, :self.stride]
+        v = v[:, :, :self.stride]
+        prev_mem = prev_mem if prev_mem is not None else torch.zeros(B, H, self.stride, self.mem_dim, device=k.device)
+        kvm = torch.cat([k, v, prev_mem], dim=-1)
+        new_mem = self.update_net(kvm)
+        
+        return new_mem
+
+    def forward_retrieve(self, q, mem=None):
+        B, H, N, D = q.shape
+        # if mem is None:
+        #     return torch.zeros_like(q)  # this only works when token dimension is the same as per head dimension
+        
+        k_mem = self.retrieve_key_proj(mem)
+        v_mem = self.retrieve_val_proj(mem)
+        x_mem = F.scaled_dot_product_attention(query=q, key=k_mem, value=v_mem) # d = dim_head
+        x_mem = self.proj_to_token_dim(x_mem) # d = token dimension (dim)
+        return x_mem
+
+    def memory_loss(self, mem, target):
+        return None
+
+
+class Attention(nn.Module):
+    def __init__(self, dim, heads = 8, dim_head = 64, is_causal = True):
+        super().__init__()
+        inner_dim = dim_head *  heads
+        self.heads = heads
+        self.scale = dim_head ** -0.5
+        self.norm = nn.LayerNorm(dim)
+        self.rotary_emb = RotaryEmbedding(dim_head)
+        self.attend = nn.Softmax(dim = -1)
+
+        self.is_causal = is_causal
+
+        self.to_qkv = nn.Linear(dim, inner_dim * 3, bias = False)
+        self.to_out = nn.Linear(inner_dim, dim, bias = False)
+        self.temporal_memory = NeuralMemory(
+            input_dim=dim_head,
+            output_dim=dim//heads, # 384//8
+            mem_dim=dim_head,
+            hidden_dim=1024,
+            stride=1
+        )
+
+    def forward(self, x, mem: Optional[torch.Tensor]):
+        """
+        if there is memory, no need for cache
+        """
+        x = self.norm(x)
+
+        B = x.shape[0]
+        qkv = self.to_qkv(x).chunk(3, dim = -1)
+        q, k, v = map(lambda t: rearrange(t, 'b n (h d) -> b h n d', h = self.heads), qkv)
+
+        # update memory
+        new_mem = self.temporal_memory.forward_update(k, v, mem)  # b, h, n, d
+        # retrieve memory from the previous chunk
+        if mem is None:
+            x_mem = torch.zeros_like(x)
+        else:
+            x_mem = self.temporal_memory.forward_retrieve(q, mem) #  b, h, n, d
+            x_mem = rearrange(x_mem, "b h n d -> b n (h d)", b=B)
+        # print('memory: ', x_mem.shape, x.shape)
+        # concatenate memory frames and current frames
+        x_tilta = torch.cat([x_mem, x], dim=1)
+        qkv = self.to_qkv(x_tilta)
+        # TODO check if using new q; if use new q, output needs to truncate [:, :x.shape[1]] to remove memory frames
+        _, k, v = map(lambda t: rearrange(t, 'b n (h d) -> b h n d', h = self.heads), qkv.chunk(3, dim=-1))
+
+        # relative positions
+        # TODO check rope k with memory
+        # q = self.rotary_emb.rotate_queries_or_keys(q, self.rotary_emb.freqs)
+        # k = self.rotary_emb.rotate_queries_or_keys(k, self.rotary_emb.freqs)
+        q, k = self.rotary_emb.rotate_queries_with_cached_keys(q, k)
+
+        # causal mask
+        if self.is_causal:
+            seq_len = q.shape[-2]
+            causal_mask = torch.triu(torch.ones((seq_len, seq_len), device=x.device), diagonal=1).bool()
+
+        # attention
+        dots = torch.matmul(q, k.transpose(-1, -2)) * self.scale
+        if self.is_causal:
+            dots.masked_fill_(causal_mask, float('-inf'))
+
+        attn = self.attend(dots)
+
+        out = torch.matmul(attn, v)
+        out = rearrange(out, 'b h n d -> b n (h d)')
+        
+        return self.to_out(out), new_mem
+
+
+class DynamicTanh(nn.Module):
+    def __init__(self, normalized_shape, channels_last=True, alpha_init_value=0.5):
+        super().__init__()
+        self.normalized_shape = normalized_shape
+        self.alpha_init_value = alpha_init_value
+        self.channels_last = channels_last
+
+        self.alpha = nn.Parameter(torch.ones(1) * alpha_init_value)
+        self.weight = nn.Parameter(torch.ones(normalized_shape))
+        self.bias = nn.Parameter(torch.zeros(normalized_shape))
+
+    def forward(self, x):
+        x = torch.tanh(self.alpha * x)
+        if self.channels_last:
+            x = x * self.weight + self.bias
+        else:
+            x = x * self.weight[:, None, None] + self.bias[:, None, None]
+        return x
+
+    def extra_repr(self):
+        return f"normalized_shape={self.normalized_shape}, alpha_init_value={self.alpha_init_value}, channels_last={self.channels_last}"
+
+
+class NeuromemTransformer(Module):
+    def __init__(
+        self, 
+        num_tokens,
+        dim, 
+        depth, 
+        heads, 
+        dim_head, 
+        mlp_dim,
+        dynamic_tanh = False,
+        token_emb: Module | None = None,
+        ):
+        super().__init__()
+        if dynamic_tanh:
+            self.norm = DynamicTanh(dim)
+        else:
+            self.norm = nn.RMSNorm(dim) # or LayerNorm
+        if not exists(token_emb):
+            token_emb = nn.Embedding(num_tokens, dim)
+
+        self.token_emb = token_emb
+
+        self.layers = nn.ModuleList([])
+        for _ in range(depth):
+            self.layers.append(nn.ModuleList([
+                Attention(dim, heads = heads, dim_head = dim_head),
+                FeedForward(dim, mlp_dim)
+            ]))
+
+        self.to_logits = LinearNoBias(dim, num_tokens)
+
+    @torch.no_grad()
+    def sample(
+        self,
+        prompt: Tensor,
+        seq_len: int,
+        temperature = 1.5,
+        filter_fn: Callable = min_p_filter,
+        filter_kwargs: dict = dict(
+            min_p = 0.1,
+        ),
+        show_progress = True,
+        ):
+        was_training = self.training
+        self.eval()
+
+        mem = None
+        assert len(prompt.shape) == 2
+
+        prompt_seq_len, out = prompt.shape[-1], prompt.clone()
+        sample_num_times = max(0, seq_len - prompt_seq_len)
+
+        with tqdm.tqdm(total = sample_num_times, disable = not show_progress) as pbar:
+            while out.shape[-1] < seq_len:
+                logits, mem = self.forward_one_step(
+                    out,
+                    mem = mem,
+                )
+
+                if not exists(logits):
+                    continue
+
+                logits = logits[:, -1]
+
+                logits = filter_fn(logits, **filter_kwargs)
+                sample = gumbel_sample(logits, temperature = temperature)
+
+                out = torch.cat((out, sample), dim = -1)
+                pbar.update(1)
+
+        self.train(was_training)
+
+        return out[..., prompt_seq_len:]
+
+    def forward_one_step(
+        self, 
+        x,
+        mem = None,
+        ):
+        x = self.token_emb(x)
+
+        x = x[:, -1:]
+        
+        if mem is None:
+            # first step
+            mem = [None] * len(self.layers)
+        for i, ((attn, ff), mem_i) in enumerate(zip(self.layers, mem)):
+            attn_out, mem[i] = attn(x, mem = mem_i)
+            x = attn_out + x
+            x = ff(x) + x
+
+        x = self.norm(x)
+        logits = self.to_logits(x)
+
+        return logits, mem
+
+    def forward(
+        self, 
+        x,
+        mem = None,
+        return_loss = False,
+        ):
+        seq_len = x.shape[1]
+        out = x[:, :1]  # only first token
+        mem = None
+        # segment_length = 30
+        # stride = segment_length // 2
+        temperature = 1.5
+        logits_track = []
+        # for start, end in zip(range(0, seq_len - segment_length + stride, stride), range(segment_length, seq_len + stride, stride)):
+        #     if end > seq_len:
+        #         end = seq_len
+        #     out = x[:, start:end]
+        #     logits, mem = self.forward_one_step(
+        #         out,
+        #         mem = mem,
+        #     )
+            
+        while out.shape[-1] < seq_len:
+            logits, mem = self.forward_one_step(
+                out,
+                mem = mem,
+            )
+
+            if not exists(logits):
+                continue
+
+            logits = logits[:, -1]
+            logits_track.append(logits)
+            # logits = filter_fn(logits, **filter_kwargs)
+            sample = gumbel_sample(logits, temperature = temperature)
+
+            out = torch.cat((out, sample), dim = -1)
+
+        if return_loss:
+            logits = torch.stack(logits_track, dim=1)
+            labels = x[:, 1:]
+            print('logits: ', logits.shape, labels.shape)
+            loss = F.cross_entropy(rearrange(logits, 'b n l -> b l n'), labels)
+            return loss 
+        else:
+            return out
